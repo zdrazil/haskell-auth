8b9fb955dda1372f334645a5d27cc87e8d8ef61b
Vladimír Zdražil <mail@vzdrazil.com>
2018-12-27 16:14:21 +0100

    Add validation

--- src/Domain/Validation.hs
+++ src/Domain/Validation.hs
@@ -10,3 +10,14 @@ validate constructor validations val =
   case concatMap (\f -> maybeToList $ f val) validations of
     []   -> Right $ constructor val
     errs -> Left errs
+
+rangeBetween :: (Ord a) => a -> a -> e -> Validation e a
+rangeBetween minRange maxRange msg val =
+  if val >= minRange && val <= maxRange then Nothing else Just msg
+
+lengthBetween :: (MonoFoldable a) => Int -> Int -> e -> Validation e a
+lengthBetween minLen maxLen msg val =
+  rangeBetween minLen maxLen msg (length val)
+
+regexMatches :: Regex -> e -> Validation e Text
+regexMatches regex msg val = if val =~ regex then Nothing else Just msg
